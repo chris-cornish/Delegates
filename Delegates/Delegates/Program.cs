79e219d3fbbc7bbb2c8a89650ed6bf06f63cd2f1
chris-cornish <csquare2390@gmail.com>
2016-10-10 09:46:17 -0400

    Pointed delegate towards new anonymous method.

--- Delegates/Delegates/Program.cs
+++ Delegates/Delegates/Program.cs
@@ -10,21 +10,21 @@ namespace Delegates
     {
         delegate void SayGreeting(string name);
 
-        public static void SayHello(string name)
-        {
-            Console.WriteLine(string.Format("Hello, {0}", name));
-        }
-
         public static void SayGoodbye(string name)
         {
             Console.WriteLine(string.Format("Goodbye, {0}", name));
         }
         static void Main(string[] args)
         {
-            SayGreeting sayGreeting = new SayGreeting(SayHello);
+            SayGreeting sayGreeting = delegate (string name)
+            {
+                Console.WriteLine(string.Format("Hello, {0}", name));
+            };
+
             Console.WriteLine("What's your name?");
             string input = Console.ReadLine();
             sayGreeting(input);
+            Console.ReadLine();
             sayGreeting = new SayGreeting(SayGoodbye);
             sayGreeting(input);
         }

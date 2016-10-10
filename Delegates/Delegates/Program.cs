ab202d453a20167b7f9e08ec19f0cada2dc24e3f
chris-cornish <csquare2390@gmail.com>
2016-10-10 09:43:49 -0400

    Added SayHello and SayGoodbye

--- Delegates/Delegates/Program.cs
+++ Delegates/Delegates/Program.cs
@@ -8,8 +8,25 @@ namespace Delegates
 {
     class Program
     {
+        delegate void SayGreeting(string name);
+
+        public static void SayHello(string name)
+        {
+            Console.WriteLine(string.Format("Hello, {0}", name));
+        }
+
+        public static void SayGoodbye(string name)
+        {
+            Console.WriteLine(string.Format("Goodbye, {0}", name));
+        }
         static void Main(string[] args)
         {
+            SayGreeting sayGreeting = new SayGreeting(SayHello);
+            Console.WriteLine("What's your name?");
+            string input = Console.ReadLine();
+            sayGreeting(input);
+            sayGreeting = new SayGreeting(SayGoodbye);
+            sayGreeting(input);
         }
     }
 }
